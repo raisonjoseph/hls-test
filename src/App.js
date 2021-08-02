843cfd2d490b26f8508c37a48f67ffa44a77a7ca
Raison Joseph <raison@entri.me>
2021-08-02 10:45:59 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Player from "./Player";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <Player />
+      </div>
+    </div>
+  );
+}
+
+export default App;
